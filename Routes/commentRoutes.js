e43785b4dd829ea17ea2599fc4e68baf06c371f8
Brian Morck <brianmorck@u.boisestate.edu>
2020-04-19 11:27:44 -0600

    Implements #37

--- Routes/commentRoutes.js
+++ Routes/commentRoutes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
+const ReturnFormat = require('../Util/ReturnFormat');
+const errorHandle = require('../Util/ErrorHandle');
 const route_start = '/comment';
 
 //Comment Route
 router.get('/comment', (req, res, next) => {
-    const response = {
-        message: "TBD - This is a comment from GitBot!"
-    }
-    res.send(response)
+	try {
+		const rf = new ReturnFormat(req);
+    		res.send(rf.format());
+	} catch(err) {
+		errorHandle(res, err, route_start);
+	}
 })
 
 module.exports = router;
